3a0e0b6ef88886f22e64d8f2573e8a72d435f034
Ayush <ayushchahat110903@gmail.com>
2024-11-14 18:19:24 +0530

    Changing Mongodb from Compus to Atlas

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,8 @@
 const mongoose = require('mongoose');
-const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log("MongoDB connected");
-    } catch (error) {
-        console.error("Database connection error:", error);
-        process.exit(1);
-    }
-};
-module.exports = connectDB;
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log("Connected to MongoDB Atlas"))
+.catch((error) => console.error("MongoDB Atlas connection error:", error));
