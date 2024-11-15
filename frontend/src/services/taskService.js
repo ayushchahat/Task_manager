aa5bf0ee6786b26699f4b801aa38aaafbe9e0c7b
Ayush <ayushchahat110903@gmail.com>
2024-11-15 07:31:49 +0530

    Done some changes in api path to resolve some reeor on frontend

--- frontend/src/services/taskService.js
+++ frontend/src/services/taskService.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
-const API_URL = 'https://task-manager-backend-oavh.onrender.com';
+const API_URL = 'https://task-manager-backend-oavh.onrender.com/api/tasks';
 
 export const fetchTasks = () => axios.get(API_URL);
 export const addTask = (task) => axios.post(API_URL, task);
 export const updateTask = (id, task) => axios.put(`${API_URL}/${id}`, task);
 export const deleteTask = (id) => axios.delete(`${API_URL}/${id}`);
+
